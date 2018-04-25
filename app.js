1c5333919aad76b83557a0dbc18f2af3694770a4
midstreamer <chadalessi@yahoo.com>
2018-04-24 20:26:43 -0700

    added tables route

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/tables.html"));
+});
+
 // -------------------------------------------- body Parser middleware
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
