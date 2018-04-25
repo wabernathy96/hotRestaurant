1aa9d0c46fba96c3b28ca3a35caeeb51d442be65
wabernathy96 <wabernathy96@gmail.com>
2018-04-24 20:11:27 -0500

    Added start to script

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ var app = express();
 //Static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get("/", function(req, res) {
+    // res.send("Welcome to the Star Wars Page!")
+    res.sendFile(path.join(__dirname, "index.html"));
+});
 
 // -------------------------------------------- body Parser middleware
 app.use(bodyParser.urlencoded({extended: false}));
