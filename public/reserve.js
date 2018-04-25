5527aa8356add1cd3c7c0b8d559871a220332b0c
wabernathy96 <wabernathy96@gmail.com>
2018-04-24 20:38:37 -0500

    Added routes dir

---
+++ public/reserve.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const path = require('path');
+// const flash = require('connect-flash');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+// const db = require('./config/database');
+var app = express();
